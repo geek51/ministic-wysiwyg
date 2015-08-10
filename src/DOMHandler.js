ac9f782ef5df2b07a7a3b5926c7f5487fa7f2100
César Díaz <cesar1951@gmail.com>
2015-08-09 20:05:38 -0500

    Added the following methods *create *first *wrap

--- src/DOMHandler.js
+++ src/DOMHandler.js
@@ -5,7 +5,7 @@ define(function(){
      * @param  Obect savedInstances Hashmap with instances already created
      * @return Array An array of nodes that will be used to create new instaces
      */
-    'getNewEditors' : function(savedInstances) {
+    getNewEditors: function(savedInstances) {
       var elements = document.getElementsByClassName('ministic-editor'),
           newElements = [];
 
@@ -13,16 +13,65 @@ define(function(){
 
         var instanceId = elements[i].id;
 
-        if (typeof instanceId == 'undefined') {
+        if (typeof instanceId == 'undefined' || instanceId.length == 0) {
           throw new Error('Each Ministic instance must have an id');
         }
 
         if (instanceId in savedInstances) continue;
 
+        // create a wrapper that will be used id DOM/Styles manipulation
+        this.wrap(elements[i], this.create('div', {'class':'ministic-wrapper', 'styles':{'position':'relative'}}));
+
         newElements.push(elements[i]);
       }
 
       return newElements;
+    },
+    /**
+     * Creates a new DOM element
+     * @param  String tagName    the tagname to create
+     * @param  Object properties DOM element propertiens
+     * @return HTMLElement
+     */
+    create: function(tagName, properties) {
+      var element = document.createElement(tagName);
+
+      for (var propertyName in properties) {
+        switch (propertyName) {
+          case 'text':
+            element.appendChild(document.createTextNode(properties[propertyName]));
+          break;
+          case 'styles':
+            var styles = properties[propertyName];
+            for (var name in styles) {
+              element.style[name] = styles[name];
+            }
+          break;
+          default:
+            element.setAttribute(propertyName, properties[propertyName]);
+        }
+
+      }
+
+      return element;
+    },
+    /**
+     * Returns the element first child ignoring text nodes
+     * @param  HTMLElement element
+     * @return Mixed the HTMLElement or false if the element is empty.
+     */
+    first: function(element){
+      if (element.firstChild.nodeType != 3) return element.firstChild;
+
+      for (var i = 0, total = element.childElementCount; i < total; i++) {
+        if (element.children[i].nodeType == 1) return element.children[i];
+      }
+
+      return false;
+    },
+    wrap: function(element, new_element) {
+      element.parentNode.insertBefore(new_element, element);
+      new_element.appendChild(element);
     }
   }
 });
