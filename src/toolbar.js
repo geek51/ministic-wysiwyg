cf86213ac8fc568f8fcd52690efd3801ab94f16b
César Díaz <cesar1951@gmail.com>
2015-08-10 21:21:53 -0500

    Initial toolbar module code

--- src/toolbar.js
+++ src/toolbar.js
@@ -1,3 +1,19 @@
-define(function(require){
-  
+define(function(require) {
+
+  return {
+    /**
+     * Inits all the toolbars for the specified toolbar
+     * @param  MinisticInsance ministicInsance
+     */
+    init: function(ministicInsance) {
+      var toolbars = ministicInsance.config.toolbars;
+
+      // async load of each toolbar
+      for (var i = 0, total = toolbars.length; i < total; i++) {
+        require(['./toolbars/toolbar-' + toolbars[i]], function(toolbar){
+          toolbar.init(ministicInsance);
+        });
+      }
+    }
+  }
 });
